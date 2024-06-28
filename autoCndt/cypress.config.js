8cd3ca727c6841e82e182c5f1fffc12fc1348a28
Adilson Fernandes <adilsonfernandes588@gmail.com>
2024-06-28 15:58:27 -0300

    criada estrutura do projeto (autoCndt -> automação do cndt / autoSefaz -> automação do sefaz / contabIA -> aplicação principal com spring)

---
+++ autoCndt/cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+const solveCaptcha = require("./cypress/support/e2e");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('task', {
+        async solveCap(){
+          var ret = await solveCaptcha();
+          console.log(ret);
+          return ret;
+        }
+
+        
+      });
+    },
+  },
+});
