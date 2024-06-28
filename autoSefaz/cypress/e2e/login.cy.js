05f36c0915125ecd59dbf30d21278235c1c6876b
Mateus Morais <mateusdeassismorais@gmail.com>
2024-06-28 18:01:08 -0300

    Adicionando repositorio autoSefaz

--- autoSefaz/cypress/e2e/login.cy.js
+++ autoSefaz/cypress/e2e/login.cy.js
@@ -2,10 +2,8 @@
 describe('Coletando notas Sefaz', () => {
   it('acessar sefaz', () => {
 
-    var tabela_notas
-    var tabela_resumo
-    tabela_notas, tabela_resumo = cy.coleta_notas()
-    cy.cria_arquivo_svc(tabela_notas, tabela_resumo)
+    cy.coleta_notas()
+    cy.cria_arquivo_json()
     
   })
   });
