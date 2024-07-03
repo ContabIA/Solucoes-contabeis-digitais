86a803b2850b6c55c83dc2f013279cf482665073
Arnaud-Neto <arnaudmacedomelo@gmail.com>
2024-07-03 19:09:13 -0300

    organizando o input e output

--- autoCndt/index.js
+++ autoCndt/index.js
@@ -1,13 +1,61 @@
-const cypress = require('cypress')
+const cypress = require('cypress');
+
 
 var runArgs = {
     spec: './cypress/e2e/certidao.cy.js', 
     configFile: './cypress.config.js', 
-    env:'cnpj=123.456.78/9101-11', 
+    env:'', 
     headed:true, 
     browser:'firefox'
-}
+};
 
-cypress.run(runArgs).then((result) => {
-    console.log('ok');
-});
+// função para pegar os cnpjs
+function getInput(){
+
+    // função temporaria para testes
+    let fs = require('fs');
+    let path = require('path');
+
+    return fs.readFileSync(path.join(__dirname, "testeInput.txt")).toString().split('\n');
+
+};
+
+// função para enviar o output
+function sendOutput(output){
+
+    // função temporaria para testes 
+    let fs = require("fs");
+    let path = require("path");
+    
+    fs.writeFileSync(path.join(__dirname, "testeOutput"), output)
+};
+
+
+// roda o cypress asyncronamente usando os argumentos em runArgs e depois retorna os resultados para o callback
+async function runCypress(runArgs, callback){
+
+    // roda o cypress utilizando os argumentos  e 
+    cypress.run(runArgs).then(callback);
+
+};
+
+// função principal do documento
+function main(){
+
+    // lista com todos os cnpjs
+    let listaCnpj = getInput();
+
+    //loop para varer os cnpjs
+    for (let i = 0; i < listaCnpj.length; i++){
+
+        // seta o valor de cnpj
+        runArgs.env = "cnpj=" + listaCnpj[i];
+
+        // run cypress/e2e/certidao.cy.js
+        runCypress(runArgs, (resultados) => {
+
+            // envia os resultados 
+            sendOutput(listaCnpj[i] + ":" + resultados.status);
+        });
+    };
+};
