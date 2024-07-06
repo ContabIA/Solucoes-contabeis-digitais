68393d31d28d7ddcdca81dac83856c4cbcfd9d95
Arnaud-Neto <arnaudmacedomelo@gmail.com>
2024-07-06 16:20:51 -0300

    atualizando as funções de input e output, para que elas pegem e enviem os dados atraves da API

--- autoCndt/cypress/e2e/certidao.cy.js
+++ autoCndt/cypress/e2e/certidao.cy.js
@@ -12,11 +12,19 @@ describe("Teste do negativado trabalhista", ()=>{
         cy.task('solveCap').then((resp)=>{
             cy.get('[name="resposta"]').type(resp);
         })
-        cy.get('[name="gerarCertidaoForm:cpfCnpj"]').type(Cypress.env('cnpj'));
+        cy.get('[name="gerarCertidaoForm:cpfCnpj"]').type("04.006.92/0001-00");
         cy.get('[name="gerarCertidaoForm:btnEmitirCertidao"]').click();
-        cy.task('writeOutput',"cnpj:"+Cypress.env('cnpj')+"\nsucesso!!!")
+        cy.task('writeOutput',"cnpj:"+Cypress.env('cnpj')+"\nsucesso!!!");
+        cy.get('#divSucesso').should('have.css', 'display', 'none').then(($element) => {
+            if ($element.css('display') != 'none') {
+                //função de retorno de resposta positiva
+            }
+        });
+        cy.get('#divErro').should('have.css', 'display', 'none').then(($element)=>{
+            if($element.css('display') != 'none'){
+                //função de retorno de resposta negativa
+            }
+        });
     });
 });
-
-
 //adicionar suporte a variáveis e wait em função do download do arquivo
