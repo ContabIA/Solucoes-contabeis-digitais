032d393fe5d2e9f3397be892be46c7dd4d9b7f04
Adilson Fernandes <adilsonfernandes588@gmail.com>
2024-07-10 16:46:00 -0300

    adicionado a versão mais atual da aplicação spring com merge

--- contabia/src/main/resources/static/js/editCnpj.js
+++ contabia/src/main/resources/static/js/editCnpj.js
@@ -1,5 +1,53 @@
-async function editEmpresa(cnpjEmpresa, cnpj){
-    var form = document.getElementById("formCadCnpj")
-    await fetch("http://localhost:8080/listaCnpj?cnpj="+cnpj+"&cnpjEmpresa="+cnpjEmpresa, {method:"PUT"})
-    location.reload();
+async function atualizaDados(cnpjUser, cnpjEmpresa){
+
+    var freqSefaz = document.getElementById("frequenciaSefaz");
+    var freqCndt = document.getElementById("frequenciaCndt");
+    var checkboxSefaz = document.getElementById("checkboxSefaz");
+    var checkboxCndt = document.getElementById("checkboxCndt");
+    var nome = document.getElementById("nomeCadCnpj");
+    var cnpjEmpresaInp = document.getElementById("cnpjCadCnpj");
+
+    let body = {
+        cnpjEmpresa : cnpjEmpresaInp.value,
+        nome : nome.value,
+        checkboxSefaz : checkboxSefaz.checked,
+        checkboxCndt : checkboxCndt.checked,
+        frequenciaSefaz : freqSefaz.value,
+        frequenciaCndt : freqCndt.value
+    }
+
+    await fetch('http://localhost:8080/editCnpj?cnpjUser='+cnpjUser+'&cnpjEmpresa='+cnpjEmpresa, {
+        method:"PUT",
+        body:JSON.stringify(body),
+        headers:{'Content-Type': 'application/json'},
+    })
+    .then((resp)=>{return resp.json()})
+    .then((respJson) => {
+        window.location = "/listaCnpj?cnpjUser="+respJson;
+    });
 }
+const formataCnpj = function (textCnpj){    
+    var cnpjFormatado = ''
+    
+    for(var i = 0; i < textCnpj.length; i++){
+
+        if(i == 2 || i == 5){
+            cnpjFormatado += '.';
+        }else if(i == 8){
+            cnpjFormatado += '/';
+        }else if(i == 12){
+            cnpjFormatado += '-';
+        }
+        cnpjFormatado += textCnpj[i]
+        
+    }
+
+    return cnpjFormatado
+}
+
+var cnpj  = document.getElementById("user")
+var textCnpj = cnpj.textContent.split('')
+var cnpjFormatado = formataCnpj(textCnpj)
+cnpj.innerHTML = cnpjFormatado  
+
+
--- contabia/src/main/resources/static/js/main.js
+++ contabia/src/main/resources/static/js/main.js
@@ -1,4 +1,4 @@
-function formataCnpj(textCnpj){
+const formataCnpj = function (textCnpj){    
     var cnpjFormatado = ''
     
     for(var i = 0; i < textCnpj.length; i++){
@@ -17,44 +17,23 @@ function formataCnpj(textCnpj){
     return cnpjFormatado
 }
 
-function formataData(data){
+const formataData = function(data){
     var dataFormatada = ''
 
-    listaData = data.split('-')
-    dataFormatada = listaData[2] + "/" + listaData[1] + "/" + listaData[0]
-
+    dataFormatada = data.split('-')[2] + "/" + data.split('-')[1] + "/" + data.split('-')[0]
+    
     return dataFormatada
 }
 
 const TRADUZ_MES = {"JANUARY" : "Janeiro", "FEBRUARY" : "Fevereiro", "MARCH" : "Março", "APRIL" : "Abril", "MAY" : "Maio", "JUNE" : "Junho", "JULY" : "Julho", "AUGUST" : "Agosto", "SEPTEMBER" : "Setembro", "OCTOBER" : "Outubro", "NOVEMBER" : "Novembro", "DECEMBER" : "Dezembro"}
 
-var cnpj  = document.getElementById("user")
-var textCnpj = cnpj.textContent.split('')
-var cnpjFormatado = formataCnpj(textCnpj)
-cnpj.innerHTML = cnpjFormatado
+const formataCnpjUser = function(){
+    var cnpj  = document.getElementById("user")
+    var textCnpj = cnpj.textContent.split('')
+    var cnpjFormatado = formataCnpj(textCnpj)
+    cnpj.innerHTML = cnpjFormatado  
+}
 
-var listSpanCnpj = document.querySelectorAll(".nomeDivCnpj")
-listSpanCnpj.forEach(spanCnpj => {
-    var textSpanCnpj = spanCnpj.textContent.split(' - ')
-    var spanCnpjFormatado = formataCnpj(textSpanCnpj[1])
-    spanCnpj.innerHTML = textSpanCnpj[0] + ' - ' + spanCnpjFormatado
-});
+formataCnpjUser()
 
-var listAltCnpj = document.querySelectorAll(".nomeAlt")
-listAltCnpj.forEach(AltCnpj => {
-    var textAltCnpj = AltCnpj.textContent.split(' - ')
-    var altCnpjFormatado = formataCnpj(textAltCnpj[1])
-    AltCnpj.innerHTML = textAltCnpj[0] + ' - ' + altCnpjFormatado + ' - ' + TRADUZ_MES[textAltCnpj[2]]
-});
-
-var tdData = document.getElementById("data")
-var data = tdData.textContent
-dataFormatada = formataData(data)
-tdData.innerHTML = dataFormatada
-
-var listaTitulos = document.querySelectorAll(".title2")
-var textoTituloCnpj = listaTitulos[0].textContent.split(" - ")
-var cnpjFormatado = formataCnpj(textoTituloCnpj[1])
-listaTitulos[0].innerHTML = textoTituloCnpj[0] + " - " + cnpjFormatado
-var mesFormatado = TRADUZ_MES[listaTitulos[1].textContent.split(" - ")[1]]
-listaTitulos[1].innerHTML = listaTitulos[1].textContent.split(" - ")[0] + " - " + mesFormatado
+export {formataCnpj, formataData, TRADUZ_MES}
