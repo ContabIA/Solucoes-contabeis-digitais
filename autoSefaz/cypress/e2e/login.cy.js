22c738cc88b872d5789622dde416ff8fc12a3b20
Jhonnata Virginio <jhonnatavieiravirginio@gmail.com>
2024-07-10 23:51:45 -0300

    Iniciando a automação do SEFAZ

--- autoSefaz/cypress/e2e/login.cy.js
+++ autoSefaz/cypress/e2e/login.cy.js
@@ -1,9 +1,17 @@
 
 describe('Coletando notas Sefaz', () => {
   it('acessar sefaz', () => {
-
-    cy.coleta_notas()
-    cy.cria_arquivo_json()
+    
+    cy.loginSefaz();
+    
+    cy.writeFile("notas.json", '{\n"runs":[')
+    for (let i = 0; i < Cypress.env("cnpjs").length; i ++){
+      cy.buscandoCnpj(i)
+      cy.cria_arquivo_json(i)
+      cy.ultimo_dado(i)
+      cy.writeFile("notas.json",",\n", {flag: "a+"})
+    }
+    cy.writeFile("notas.json", "\n]\n}", {flag: "a+"});
     
   })
-  });
+});
--- autoSefaz/cypress/support/commands.js
+++ autoSefaz/cypress/support/commands.js
@@ -1,24 +1,39 @@
-Cypress.Commands.add('coleta_notas', () => {
-
+Cypress.Commands.add("loginSefaz", () => {
     //Logando no Sefaz
     cy.visit('https://www4.sefaz.pb.gov.br/atf/seg/SEGf_LoginSERVirtual.jsp', {failOnStatusCode: false})
     cy.get('[name=edtNoLogin]').type('***REMOVED***');
     cy.get('[name=edtDsSenha]').type('***REMOVED***');
     cy.get('[name=btnAvancar]').click()
+})
+
+Cypress.Commands.add('buscandoCnpj', (indexCnpj) => {
 
     //Inserindo dados das consultas
     cy.visit('https://www4.sefaz.pb.gov.br/atf/fis/FISf_ConsultarNFeXml2.do?idSERVirtual=S&h=https://www.sefaz.pb.gov.br/ser/servirtual/credenciamento/info', {failOnStatusCode: false})
+    
+    cy.get('[name=edtDtInicial]').clear()
     cy.get('[name=edtDtInicial]')
-    .type('01052024')
+    .type('08072024')
+
+    cy.get('[name=edtDtFinal]').clear()
     cy.get('[name=edtDtFinal]')
-    .type('31052024')
+    .type(new Date().toLocaleDateString())
     cy.get('[name=cmbTpDoccmpDest]')
     .select(1)
 
     cy.iframe('[name=cmpDest]')
     .as('iframe')
+    .find('[name=hidNrDocumentocmpDest]').clear()
+    cy.iframe('[name=cmpDest]')
+    .as('iframe')
     .find('[name=hidNrDocumentocmpDest]') 
-    .type('09197286000111')
+    .type(Cypress.env('cnpjs')[indexCnpj])
+
+    cy.iframe('[name=cmpDest]')
+    .as('iframe')
+    .find('[name="hidNoHumanoInstcmpDest"]') 
+    .clear()
+    
 
     cy.get('@iframe')
     .find('[name=btnPesquisar]')
@@ -29,20 +44,21 @@ Cypress.Commands.add('coleta_notas', () => {
     cy.get('[name=btnConsultar]').click()
 
     cy.wait(1000)
-    
-
-    
 })
 
-Cypress.Commands.add('cria_arquivo_json', () => {
-    cy.writeFile('notas.txt', '')
+let valores = []
+
+Cypress.Commands.add('cria_arquivo_json', (indexCnpj) => {
+    cy.writeFile('notas.json', '{' + '\n' + '"listaNotas" : [' + '\n', {flag:"a+"})
+
     
     //Coletando tabela com notas e tabela com resumo das notas 
 
     var c = 0
+    var len = 0
+
     cy.get('[name=frmConsultar] > table:nth-child(2) > tbody > tr > td')
     .each(($coluna, index) => {
-
         if (index < 8){
             c += 1
             return
@@ -56,66 +72,46 @@ Cypress.Commands.add('cria_arquivo_json', () => {
             return
         } 
         cy.get($coluna).invoke('text').then(($conteudo) => {
-            if (index == c - 1){
-                cy.writeFile('notas.txt', $conteudo  + '\n', {flag :'a+'})
-            } else{
-                cy.writeFile('notas.txt', $conteudo  + ', ', {flag :'a+'})
-            }
             
+            if (len == 6){
+                let registro = {
+                id: valores[0].trim(),
+                serie: valores[1].trim(),
+                data: valores[2].trim(),
+                nomeEmitente: valores[3].trim(),
+                situacao: valores[4].trim(),
+                valor: valores[5].trim(),
+                cnpjEmpresa: Cypress.env('cnpjs')[indexCnpj]
+                }
+                cy.writeFile('notas.json', registro, {flag: 'a+'})
+                cy.writeFile('notas.json', ',' + "\n", {flag: 'a+'})
+                
+                len = 1
+                valores = []
+                valores.push($conteudo)
+            } else { 
+                len = len + 1
+                valores.push($conteudo)
+            }
         })
         if (index <= 1){
             return
         }
-        
-        // var conteudo = ''
-        // cy.get($linha).find('td').each(($coluna, k, $lista) => {
-
-        //     // cy.get($lista).invoke('text').then(($conteudo) => {
-        //     //     cy.writeFile('notas.txt', $conteudo + '\n', {flag : 'a+'})
-        //     // })
-
-        //     // return false
-        //     if (k==0){
-        //         return
-        //     }
-
-            
-        //     cy.get($coluna).invoke('text').then(($conteudo) => {
-        //         conteudo = $conteudo + ','
-        //         if (k == $lista.length - 1){
-        //             conteudo = $conteudo + '\n'
-        //         }
-                
-        //         cy.writeFile('notas.txt', conteudo, {flag : 'a+'})
-        //     })
-            
-
-            
-        // })
-        
-        
-        
-
-
-        // cy.get($linha).find('td').each(($coluna) => {
-        //     cy.get($coluna)
-        // })
-    }) 
-
-    
-    
+    })
 
 })
 
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('ultimo_dado', (indexCnpj) => {
+    let registro = {
+        id: valores[0].trim(),
+        serie: valores[1].trim(),
+        data: valores[2].trim(),
+        nomeEmitente: valores[3].trim(),
+        situacao: valores[4].trim(),
+        valor: valores[5].trim(),
+        cnpjEmpresa: Cypress.env('cnpjs')[indexCnpj]
+    }
+    cy.writeFile('notas.json', registro, {flag: 'a+'})
+    cy.writeFile('notas.json', "\n" + ']' + '\n' + '}', {flag: 'a+'})
+})
