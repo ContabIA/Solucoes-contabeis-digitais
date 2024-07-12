f3f559c70ca04ed2cb28010a346ad4254be8aec5
Mateus Morais <mateusdeassismorais@gmail.com>
2024-07-12 17:00:28 -0300

    Tratando erros de cadastro de usário

--- contabia/src/main/java/com/contabia/contabia/controllers/RegUserController.java
+++ contabia/src/main/java/com/contabia/contabia/controllers/RegUserController.java
@@ -1,6 +1,12 @@
 package com.contabia.contabia.controllers;
 
+import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,7 +20,7 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/cadastro")
@@ -27,11 +33,41 @@ public class RegUserController {
     public String cadastro() {
         return "cadastroUser";
     }
-    
+
     @PostMapping
-    @Transactional
-    public String addUsuario(@Valid UserDto dados){
-        userRepository.save(new UserModel(dados));
-        return "redirect:/";
+    public ResponseEntity<Map<String, String>> addUsuario(@Valid @RequestBody UserDto dados) {
+
+        Map<String, String> resp = new HashMap<>();
+
+        try {
+            userRepository.save(new UserModel(dados));
+            
+            resp.put("resp", "deu bom");
+            return ResponseEntity.ok().body(resp);
+
+        } catch (DataIntegrityViolationException e) {
+            Optional<UserModel> userByCnpj = userRepository.findByCnpj(dados.cnpj());
+            Optional<UserModel> userByEmail = userRepository.findByEmail(dados.email());
+            Optional<UserModel> userByUserSefaz = userRepository.findByUserSefaz(dados.userSefaz());
+
+            if (userByCnpj.isPresent()) {
+                resp.put("resp", "O CNPJ informado já está cadastrado no ContabIA.");
+            }
+
+            else if (userByEmail.isPresent()) {
+                resp.put("resp", "O E-mail informado já está cadastrado no ContabIA.");
+            }
+
+            else if (userByUserSefaz.isPresent()) {
+                resp.put("resp","O usuário Sefaz informado já está cadastrado no ContabIA." );
+            }
+
+            else {
+                resp.put("resp", "Erro ao cadastrar usuário. Confira as informações e tente novamente.");
+                
+            }
+
+            return ResponseEntity.status(400).body(resp);
+        }
     }
 }
