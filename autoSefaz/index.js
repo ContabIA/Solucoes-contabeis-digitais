d7aff9ed046416c2da4a4f955808dfd0393afb1d
Mateus Morais <mateusdeassismorais@gmail.com>
2024-07-22 17:40:22 -0300

    Criando rota getDadosLogin para mandar para automação dados do login sefaz, e teminando auto_sefaz

--- autoSefaz/index.js
+++ autoSefaz/index.js
@@ -5,7 +5,6 @@ const path = require("path");
 async function enviarDados(dados){
     return new Promise(async (resolve, reason) => {
       
-
       for (let i = 0; i < dados["runs"].length; i++){
         await fetch("http://localhost:8080/service/respSefaz", {
           method: "POST",
@@ -69,14 +68,12 @@ async function getInput(){
   return listaCnpj
 }
 
-
-
-function getObjDadosLogin(listaCnpj){
+async function getObjDadosLogin(listaCnpj){
   var objDadosLogin = new Object;
   for (let i = 0; i < listaCnpj.length; i++){
-    // let dadosLogin = await fetch("http://localhost:8080/service/loginSefaz?cnpjEmpresa="+listaCnpj[i]);
-    // let respJson = await dadosLogin.json();
-    let infoLogin = ["***REMOVED***", "***REMOVED***"]; // respJson.body()
+    let dadosLogin = await fetch("http://localhost:8080/service/getDadosLogin?cnpjEmpresa="+listaCnpj[i]);
+    let respJson = await dadosLogin.json();
+    let infoLogin = respJson.dadosLogin[0].split(",");
     if (objDadosLogin[infoLogin[0]] == null){
       objDadosLogin[infoLogin[0]] = [infoLogin[1], listaCnpj[i]]
     } else {
@@ -86,46 +83,50 @@ function getObjDadosLogin(listaCnpj){
   return objDadosLogin
 }
 
+async function runAuto(listaCnpj){
+  let dateObj = new Date()
+  getObjDadosLogin(listaCnpj).then((objDadosLogin) => {
+    cypress
+    .run({
+      spec: './cypress/e2e/login.cy.js',
+      env: {"dadosLogin" : objDadosLogin, diaUm : (dateObj.getDay() === 1)},
+      headed: true,
+      browser: "electron",
+    })
+    .then(() => {
+      let dadosJson = JSON.parse(fs.readFileSync(path.join(__dirname, "/notas.json")));
+
+      return (enviarDados(dadosJson));
+    })
+    // .catch((err)  => {
+    //   return err;
+    // })
+  })
+}
+
 async function main(){
   
-  // getInput().then((listaCnpj)=> {
-  //   var objDadosLogin = getObjDadosLogin(listaCnpj) 
-  //     cypress
-  //     .run({
-  //       spec: './cypress/e2e/login.cy.js',
-  //       env: {"dadosLogin" : objDadosLogin, diaUm : (dateObj.getDay() === 1)},
-  //       headed: true,
-  //       browser: "electron",
-  //     })
-  //     .then((results) => {
-  //       console.log(results)
-  //       let dadosJson = JSON.parse(fs.readFileSync(path.join(__dirname, "/notas.json")));
-
-  //       return (enviarDados(dadosJson));
-  //     })
-  //     .catch((err)  => {
-  //       console.error(err)
-  //     })
-  //   })
+  if (dateObj.getDay() === 1){
+    let listaCnpj = new Array()
+    let req = await fetch("http://localhost:8080/service/getCnpj?tamanhoFinal=1&ultimoDigito=&frequencia=&tipoConsulta=1");
+    let reqJson = await req.json();
+    listaCnpj = listaCnpj.concat(reqJson["cnpjs"]);
+    console.log(listaCnpj)
+    await runAuto(listaCnpj)
+    
+  } 
+  getInput().then(async (listaCnpj)=> {
+    if (listaCnpj.length === 0){
+      return 'no entrys'
+    }
+    console.log(listaCnpj)
+    return await runAuto(listaCnpj)
+    .catch((err) => {
+      return 'oi'
+    })
+     
+    })
   
-    var objDadosLogin = getObjDadosLogin(["09157801000130"]) 
-    var dateObj = new Date();
-      cypress
-      .run({
-        spec: './cypress/e2e/login.cy.js',
-        env: {"dadosLogin" : objDadosLogin, diaUm : (dateObj.getDay() === 1)},
-        headed: true,
-        browser: "electron",
-      })
-      .then((results) => {
-        console.log(results)
-        let dadosJson = JSON.parse(fs.readFileSync(path.join(__dirname, "/notas.json")));
-
-        return (enviarDados(dadosJson));
-      })
-      .catch((err)  => {
-        console.error(err)
-      })
 }
   
 main()
