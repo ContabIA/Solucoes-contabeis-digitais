ff76aba0162e8a8a4073d1af3be860a30c59bd65
Arnaud-Neto <arnaudmacedomelo@gmail.com>
2025-03-13 09:37:05 -0300

    evoluindo - debug - services/operationHandlerService.py > operations_in_sequence

--- automation/src/services/operationHandlerService.py
+++ automation/src/services/operationHandlerService.py
@@ -4,6 +4,10 @@ async def operations_in_sequence(run_args : dict):
     operations_list = run_args.get("operations")
     
     for o in operations_list:
+        
+        if run_args.get("debug"):
+            print("sevices/operationHandlerService.py - operations_in_sequence - runing operation:", o)
+        
         operation = operationsDict.get(o)(run_args)
         await operation.test()
 
